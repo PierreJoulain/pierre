6e20b4bc2191d1dea9fde53600ade11e39a4825e
JOULAIN <joulain@MacBook-Air-de-JOULAIN.local>
2023-06-01 17:13:32 +0200

    order by post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,17 +12,14 @@ class PostController extends Controller
 
     public function index()
     {
-        $posts = Post::all();
-        return view('articles', [
-            'posts' => $posts
-        ]);
+        //$posts = Post::all();
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('articles', ['posts' => $posts]);
     }
 
     public function show($id){
         $post = Post::findOrFail($id);
-        return view('article', [
-            'post' => $post
-        ]);
+        return view('article', ['post' => $post]);
     }
 
     public function create(){
@@ -117,5 +114,7 @@ class PostController extends Controller
         }
         return redirect()->route("welcome",$post->id);
     }
+
+
 }
 
